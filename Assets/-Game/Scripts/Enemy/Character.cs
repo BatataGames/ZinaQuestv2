7c297aacb1157c949afd07d96606988eefb0d9a2
BatataGames <murloc.dev.gaming@gmail.com>
2024-01-04 14:03:15 +0100

    Ajout de la class Enemy

---
+++ Assets/-Game/Scripts/Enemy/Character.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [System.Serializable]
+    public class PNJ
+    {
+        public int health;
+        public int healthMax;
+
+        public int damage;
+
+    }
+
+    public PNJ pnj;
+
+
+
+
+
+}
